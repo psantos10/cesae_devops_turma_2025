74ac59b4caee1119fb99ea99833c675521ea105e
Patrício dos Santos <hello@psantos.dev>
2025-05-09 22:00:49 +0100

    Adicionando mais dois arquivos.

---
+++ 01_variaveis.py
@@ -0,0 +1,31 @@
+nome = "João"
+sobrenome = 'Silva'
+
+idade = 30
+altura = 1.75
+
+viveNoPorto = True
+temCarro = False
+
+nome1, sobrenome1 = "Maria", "Oliveira"
+
+soma = 10 + 5
+valor1 = 25
+valor2 = 25
+soma = valor1 + valor2
+
+print(nome)
+print(sobrenome)
+print(idade)
+print(soma)
+
+# type(X)
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(temCarro))
+
+idade_str = str(idade)
+print(type(idade_str))
+
+# https://peps.python.org/pep-0008/ 
---
+++ 02_strings.py
@@ -0,0 +1,21 @@
+# nome = "João da Silva"
+# morada = 'Rua das Flores, 123'
+# idade = 28
+
+# PI = 3.14
+
+# print("Nome: " + nome)
+# print("Morada: " + morada)
+
+# #print("O " + nome + " tem " + str(idade) + "anos de idade")
+# print('O {nome} tem {idade} anos de idade'.format(nome=nome,idade=idade))
+# print(f'O {nome} tem {idade} anos de idade')
+
+# Metódos de Strings
+nome_completo = "marta de jesus"
+print(nome_completo.capitalize())
+# nome_completo = nome_completo.upper()
+print(nome_completo.upper())
+print(nome_completo.lower())
+# print(nome_completo.____)
+
