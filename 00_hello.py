0b38522702c89884a2fa1c52b0d86c52d9eeeaa4
Patrício dos Santos <hello@psantos.dev>
2025-05-09 21:57:43 +0100

    Primeiro arquivo python

---
+++ 00_hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Meu primento programa em Python
+print("Olá mundo")
+
+'''
+Este é um comentário
+de multiplas linhas
+'''
+
+"""
+Este é um outro comentário
+de múltiplas linhas
+"""
